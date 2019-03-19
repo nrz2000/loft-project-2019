3f1fe3f7881834953b29f4dc3bb0142997f412b4
Nikita <nrz2000@mail.ru>
2019-03-19 23:20:39 +0300

    Rename public path

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProductionBuild = argv.mode === "production";
-  const publicPath = '/nrz2000.github.io';
+  const publicPath = '/';
 
   const pcss = {
     test: /\.(p|post|)css$/,
