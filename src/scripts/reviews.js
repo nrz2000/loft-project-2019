407ce2dc93d7914ba9fbc22c42ec036027083688
Nikita <nrz2000@mail.ru>
2019-05-03 17:37:33 +0300

    Add scroll

--- src/scripts/reviews.js
+++ src/scripts/reviews.js
@@ -1,5 +1,8 @@
 import Vue from "vue";
 import Flickity from 'vue-flickity';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://webdev-api.loftschool.com/';
 
 new Vue({
   el: '#reviews',
@@ -9,9 +12,13 @@ new Vue({
 
   data() {
     return {
+      reviews: [],
+      render: false,
       flickityOptions: {
         prevNextButtons: false,
         pageDots: false,
+        wrapAround: true,
+        resize: true,
         groupCells: true
       }
     }
@@ -23,6 +30,37 @@ new Vue({
     
     previous() {
       this.$refs.flickity.previous();
-    }
+    },
+
+    async fetchReviews() {
+      try {
+        const response = await axios.get('/reviews/137');
+        this.reviews = response.data;
+        return response;
+      } catch (error) {
+        throw new Error(
+          error.response.data.error || error.response.data.message
+        )
+
+      }
+    },
+    makeArrWithRequiredImages(data) {
+      return data.map((item, index) => {
+        const requiredPic = (`https://webdev-api.loftschool.com/${item.photo}`);
+        item.photo = requiredPic;
+        // item.index = index;
+
+        return item;
+      })
+    },
   },
+  async created() {
+    try {
+      await this.fetchReviews();
+    } catch (error) {
+      console.log('error on load works');
+    }
+    this.reviews = this.makeArrWithRequiredImages(this.reviews);
+    this.render = true;
+  }
 });
